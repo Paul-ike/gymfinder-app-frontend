64980daf648c47eedfd2359b23f81b3c206fd604
ehachi <ehajianto@gmail.com>
2023-04-01 17:50:02 +0300

    homepage,gymcard,usercard,homecss,cardcss

---
+++ src/components/GymCard/GymCardUser.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./GymCard.css";
+
+const GymCardUser = ({ gym }) => {
+  return (
+    <div
+      id="gymCard"
+      className="card col- m-2"
+      // style={{ width: "18rem" }}
+    >
+      <img
+        // style={{ width: "15rem" }}
+        src={gym.image}
+        className="card-img-top img"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">Name: {gym.name}</h5>
+        <p className="card-text">Location: {gym.location}</p>
+        <Link to={`/gyms/${gym.id}`} className="btn btn-outline-dark btn-sm">
+          View
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default GymCardUser;
